240b2e771f2d75ef4c0d3af097318f6fb9006dd2
Wenderson Farias <wendersonpfarias@gmail.com>
2023-10-16 00:32:14 -0400

    conversor User para UserDTO

--- user-api/src/main/java/com/wenderson/java/back/end/dto/UserDTO.java
+++ user-api/src/main/java/com/wenderson/java/back/end/dto/UserDTO.java
@@ -2,6 +2,8 @@ package com.wenderson.java.back.end.dto;
 
 import java.time.LocalDateTime;
 
+import com.wenderson.java.back.end.model.User;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,5 +28,17 @@ public class UserDTO {
 	private		String			email;
 	private		String			telefone;
 	private		LocalDateTime	dataCadastro;
+	
+	public static UserDTO convert(User user) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setNome(user.getNome());
+		userDTO.setCpf(user.getCpf());
+		userDTO.setEmail(user.getEmail());
+		userDTO.setEndereco(user.getEndereco());
+		userDTO.setTelefone(user.getTelefone());
+		userDTO.setDataCadastro(user.getDataCadastro());
+		
+		return userDTO;
+	}
 	
 }
