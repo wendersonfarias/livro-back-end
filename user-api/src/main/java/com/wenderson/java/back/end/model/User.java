545a385aff39b69f82de8fef553b570677891851
Wenderson Farias <wendersonpfarias@gmail.com>
2023-10-16 02:06:05 -0400

    anotacao de id e geracao de id

--- user-api/src/main/java/com/wenderson/java/back/end/model/User.java
+++ user-api/src/main/java/com/wenderson/java/back/end/model/User.java
@@ -4,7 +4,11 @@ import java.time.LocalDateTime;
 
 import com.wenderson.java.back.end.dto.UserDTO;
 
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +22,8 @@ import lombok.Setter;
 @Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private 	Long id;
 	private 	String nome;
 	private		String	cpf;								
